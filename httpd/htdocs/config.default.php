792c15a93e23f9981e881edbd2b6ec8e51b4254e
Angel Bravo <luisnglbrv@gmail.com>
2021-04-26 19:59:50 -0500

    Rev1

--- httpd/htdocs/config.default.php
+++ httpd/htdocs/config.default.php
@@ -27,7 +27,7 @@ define('PLUGIN_API_URL', 'https://kanboard.org/plugins.json');
 // Enable/Disable plugin installer (Disabled by default for security reasons)
 // There is no code review or any approval process to submit a plugin.
 // This is up to the Kanboard instance owner to validate if a plugin is legit.
-define('PLUGIN_INSTALLER', false);
+define('PLUGIN_INSTALLER', true);
 
 // Available cache drivers are "file" and "memory"
 define('CACHE_DRIVER', 'memory');
